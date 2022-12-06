129095a08be2d664c17da85cfb2ae39c5954ab99
Alberto Luna <telpalbrox@users.noreply.github.com>
2022-12-06 18:41:39 +0000

    Day 1 part 2

--- day1.c
+++ day1.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "day1.h"
 
 long get_number_from_line(const char *line) { return strtol(line, NULL, 10); }
 
 int is_new_elf(const char *line) { return strlen(line) == 1; }
 
+void update_top_elfs_calories(long** top_elfs_calories, long new_calories) {
+  for (size_t index = 0; index < TOP_SIZE; index++) {
+    long calories = top_elfs_calories[index];
+    // printf("iterating over top_elfs_calories, %d, %ld, %ld\n", index, calories, new_calories);
+    if (new_calories > calories) {
+      top_elfs_calories[index] = new_calories;
+      // We need to recalculate just in case the previous calories are stil insde the top.
+      update_top_elfs_calories(top_elfs_calories, calories);
+      break;
+    }
+  }
+}
+
 void day1(void) {
   FILE *input = fopen("./input1.txt", "r");
+  long top_elfs_calories[TOP_SIZE] = {0};
   char buffer[100];
   char *line;
   long elf_index = 0;
@@ -16,6 +31,7 @@ void day1(void) {
   long current_calories = 0;
   while ((line = fgets(buffer, sizeof buffer, input))) {
     if (is_new_elf(line)) {
+      update_top_elfs_calories(top_elfs_calories, current_calories);
       if (current_calories > max_calories) {
         max_calories = current_calories;
         elf_with_max_calories = elf_index;
@@ -29,4 +45,12 @@ void day1(void) {
   }
   printf("Max calories: %ld\n", max_calories);
   printf("From elf number: %ld\n", elf_with_max_calories);
+  printf("Top:\n");
+  long total_top_calories = 0;
+  for (size_t index = 0; index < TOP_SIZE; index++) {
+    long calories = top_elfs_calories[index];
+    printf("Position %d: %ld\n", index, calories);
+    total_top_calories = total_top_calories + calories;
+  }
+  printf("Top calories sum: %ld\n", total_top_calories);
 }
--- day1.h
+++ day1.h
@@ -1,3 +1,4 @@
 #pragma once
 
+#define TOP_SIZE 3
 void day1(void);
