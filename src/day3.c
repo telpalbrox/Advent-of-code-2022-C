10737dae8fd6ed5bcb32428f4335aed19f4aecbb
Alberto Luna <alberto@albertoluna.es>
2022-12-07 16:36:37 +0000

    Day 3 part 2

--- src/day3.c
+++ src/day3.c
@@ -1,4 +1,6 @@
+#include "day3.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int get_priority_for_character(char c) {
@@ -25,18 +27,61 @@ char find_duplicated(char *rucksack) {
   return '\0';
 }
 
+char find_common_item(char **rucksack_group) {
+  char *first_rucksack = rucksack_group[0];
+  char found = 0;
+  for (char *item = &first_rucksack[0]; *item && *item != '\n'; item++) {
+    int not_found_item = 0;
+    while (!found && !not_found_item) {
+      for (size_t rucksack_index = 1; rucksack_index < RUCKSACK_GROUP_SIZE;
+           rucksack_index++) {
+        char *rucksack = rucksack_group[rucksack_index];
+        if (strchr(rucksack, *item)) {
+          found = *item;
+        } else {
+          not_found_item = 1;
+          found = 0;
+          break;
+        }
+      }
+    }
+  }
+  if (found == 0) {
+    fprintf(stderr, "Common badge not found!\n");
+    abort();
+  }
+  return found;
+}
+
+char *copy_str(char *str) {
+  unsigned long str_len = strlen(str);
+  char *new_str = (char *)malloc(str_len + 1);
+  strncpy(new_str, str, str_len + 1);
+  return new_str;
+}
+
 void day3(void) {
   FILE *input = fopen("./inputs/input3.txt", "r");
   char *line;
   char buffer[100];
+  char *rucksack_group[RUCKSACK_GROUP_SIZE];
+  int index = 0;
   int priority_sum = 0;
+  int common_badges_sum = 0;
   while ((line = fgets(buffer, sizeof buffer, input))) {
-    // line = "vJrwpWtwJgWrhcsFMMfFFhFp";
     char c = find_duplicated(line);
     priority_sum = priority_sum + get_priority_for_character(c);
-    // printf("duplicated: %c, priority: %d\n", p,
-    // get_priority_for_character(p));
+    size_t group_index = index % RUCKSACK_GROUP_SIZE;
+    rucksack_group[group_index] = copy_str(line);
+    if (group_index == RUCKSACK_GROUP_SIZE - 1) {
+      char item = find_common_item(rucksack_group);
+      for (size_t i = 0; i < RUCKSACK_GROUP_SIZE; i++) {
+      }
+      common_badges_sum = common_badges_sum + get_priority_for_character(item);
+    }
+    index++;
   }
   fclose(input);
   printf("priority_sum: %d\n", priority_sum);
+  printf("common_badges_sum: %d\n", common_badges_sum);
 }
--- src/day3.h
+++ src/day3.h
@@ -1,3 +1,4 @@
 #pragma once
 
+#define RUCKSACK_GROUP_SIZE 3
 void day3(void);
